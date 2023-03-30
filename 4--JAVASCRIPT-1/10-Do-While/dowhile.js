8c76c7cc01a56a5a8a40fb192ac8649400b10e5d
Nuno Rocha <nuno.teles.rocha@gmail.com>
2023-03-30 07:51:26 +0100

    adicionar novos ex

---
+++ 4--JAVASCRIPT-1/10-Do-While/dowhile.js
@@ -0,0 +1,9 @@
+let velocidade = 0;
+
+do {
+    alert(`A velocidade do carro é ${velocidade} km/h`);
+    velocidade-= 20;
+}   while (velocidade > 0)
+
+alert("Velocidade final: " + velocidade + " Km/h" );
+
---
+++ 4--JAVASCRIPT-1/11-Ex5-Menu-Interativo/menu.js
@@ -0,0 +1,9 @@
+let opcao = "";
+
+do {
+    opcao = prompt("Escolha uma das seguintes opçoes: \n\n 1 - Escolher \n 2 - Pedir \n 3 - Recolher \n 4 - Pagar \n 5 - Sair\n");
+    alert(`A opcao escolhilha foi a - ${opcao}`);
+    if (opcao === 5) {
+        alert("O pedido foi dado como entergue, volte sempre.");
+    }
+} while (opcao !== "5");
